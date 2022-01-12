0b2dac42ca9f77b367e5f9930356f959f6c3e207
evij-g <evgenij@evgenijgottfried.de>
2022-01-12 17:32:46 +0100

    removed unused comments

--- public/scripts/fade-header.js
+++ public/scripts/fade-header.js
@@ -8,28 +8,20 @@ function fadeOutOnScroll(element) {
     return;
   }
   var opacity = 1;
-  //console.log("window.pageYOffset",window.pageYOffset);
-  //console.log("element.getBoundingClientRect().top",element.getBoundingClientRect().top);
   var distanceToTop = window.pageYOffset + element.getBoundingClientRect().top;
 
   var elementHeight = element.offsetHeight;
   var scrollTop = document.documentElement.scrollTop;
-  //console.log("elementHeight",elementHeight, "opacity",opacity, "distanceToTop",distanceToTop,"scrollTop", scrollTop);
   
   let sT="scrollTop= "+scrollTop;
-  console.log(sT+" distanceToTop= "+distanceToTop+" scrollTop-distanceToTop= "+(scrollTop - distanceToTop));
-  
+ 
   if (scrollTop <= elementHeight) {
-   // opacity = 1 - (elementHeight-distanceToTop) / elementHeight;
+
     opacity = (elementHeight-distanceToTop) / elementHeight;
     console.log("test",opacity);
     element.style.opacity = opacity;
   }
   
-  // if (opacity <= 0) {
-  //   console.log("<=0");
-  //   element.style.opacity = opacity;
-  // }
 }
 
 function scrollHandler() {
