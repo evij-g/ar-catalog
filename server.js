b4923df3f132f373294af71bd31709d1be63c2aa
evij-g <evgenij@evgenijgottfried.de>
2022-12-09 16:33:18 +0100

    trying to copy from tutorial

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ app.listen(PORT, () => console.log(`Server is running on Port: ${PORT}`));
 
 if (process.env.NODE_ENV === 'production') {
     //*Set static folder up in production
-    app.use(express.static('/'));
+    app.use(express.static('client/build'));
 
-    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'public')));
+    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
+  
   }
