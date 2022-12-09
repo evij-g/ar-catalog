1adefb63732b3320ed29938b75faa16614444075
evij-g <evgenij@evgenijgottfried.de>
2022-12-09 18:09:37 +0100

    back to previous version

--- server.js
+++ server.js
@@ -2,10 +2,3 @@ const app = require('./app')
 const { PORT } = process.env;
 
 app.listen(PORT, () => console.log(`Server is running on Port: ${PORT}`));
-
-if (process.env.NODE_ENV === 'production') {
-    //*Set static folder up in production
-    app.use(express.static('/'));
-
-    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'public')));
-  }
