b49b11147c327cc7e224d1602585cddcc55d669e
evij-g <evgenij@evgenijgottfried.de>
2022-12-09 15:32:12 +0100

    optimize for "render"

--- server.js
+++ server.js
@@ -2,3 +2,10 @@ const app = require('./app')
 const { PORT } = process.env;
 
 app.listen(PORT, () => console.log(`Server is running on Port: ${PORT}`));
+
+if (process.env.NODE_ENV === 'production') {
+    //*Set static folder up in production
+    app.use(express.static('client/build'));
+
+    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
+  }
